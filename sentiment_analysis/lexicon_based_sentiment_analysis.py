6dff4bfb430c12aa9e7ad8aec13b261bf691a705
Tung Dao <daothanhtung94@gmail.com>
2017-02-24 16:35:32 -0500

    Update lexicon_based_sentiment_analysis.py

--- sentiment_analysis/lexicon_based_sentiment_analysis.py
+++ sentiment_analysis/lexicon_based_sentiment_analysis.py
@@ -1,3 +1,31 @@
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+                    WORK FLOW EXPLANATION
+1. The General Inquirer Dictionary is import, the dictionary contains 
+    words of different level of positivity and negativity, upon which 
+    the reviews' sentiment is assessed.
+
+2. After the dictionary was imported, the positive and negative words
+    of different senses are combined into definitive polars of either
+    positivity or negativity.
+
+3. The airline_review csv file which contains the content of the review
+    is then imported and read. The reviewcont is then tokenized, stemmed.
+    Stop words are removed. Processed words then are written into a list.
+    
+4. Each word in the list are then compared to positive word list and
+    negative word list. The number of negative words and positive words
+    in each review are counted.
+    
+5. The sentiment of the review are calculated by subtract the number of
+    negative words from the number of positive words. A negative number
+    indicate negative sentiment, a positive number indicates otherwise.
+    
+5. Logistic Regression analysis is done to find out the dependency of
+    recommended (whether the review author is going to recommend the
+    airlines) and the sentiment of the review.
+    Logistic Regression is chosen, because it's used for categorical
+    variable (binary)
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 import pandas as pd
 import csv 
 from collections import Counter
@@ -14,17 +42,21 @@ negative = []
 with open("general_inquirer_dict.txt") as fin:
     reader = csv.DictReader(fin, delimiter = '\t')
     for i, line in enumerate(reader):
+        '''Combining positive and negative words of different senses into 
+        definitive polars (neg or pos)'''
         if line['Negativ'] == 'Negativ':
             if line['Entry'].find('#') == -1:
                 negative.append(line['Entry'].lower())
             if line['Entry'].find('#') != -1:
-                negative.append(line['Entry'].lower()[:line['Entry'].index('#')])
+                negative.append(line['Entry'].
+                                lower()[:line['Entry'].index('#')])
                 
         if line['Positiv'] == 'Positiv':
             if line['Positiv'].find('#') == -1:
                 positive.append(line['Entry'].lower())
             if line['Entry'].find('#') != -1:
-                positive.append(line['Entry'].lower()[:line['Entry'].index('#')])
+                positive.append(line['Entry'].
+                                lower()[:line['Entry'].index('#')])
 
 fin.close()                
 
@@ -32,7 +64,7 @@ fin.close()
 pvocabulary = sorted(list(set(positive)))
 nvocabulary = sorted(list(set(negative)))
 
-# Import review data from csv file
+'''Import airline reviews from csv file'''
 review = pd.read_csv('airline_review.csv')
 review.columns.values
 review['reviewcontent']
@@ -43,7 +75,7 @@ review['lsentiment'] = 0
 review_index = 0
 
 
-# Tokenizing text and write to list
+''' Breaking sentences down into individual words (tokenizing)'''
 def getWordList(text, word_proc = lambda x:x):
     word_list = []
     for sent in sent_tokenize(text):
@@ -60,12 +92,13 @@ lsenti_list = []
 for text in review['reviewcontent']:
     vocabulary = getWordList(text, lambda x:x.lower())
     
-    #Remove word with 1 letter
+    #Removing words with only 1 letter
     vocabulary = [word for word in vocabulary if len(word) > 1]
-    #Remove stop word
+    #Remove stop words
     vocabulary = [word for word in vocabulary
                   if not word in stopwords.words('english')]
     
+    # Stemming words, i.e. grouping same words of different tenses or forms 
     vocabulary= [stemmer.stem(word) for word in vocabulary]
 
     pcount = 0
