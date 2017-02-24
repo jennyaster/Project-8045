439f8e3d0ba6fa5cabe1ff60c2c836005f98d5fd
hchiao1 <hchiao1@uga.edu>
2017-02-23 21:10:06 -0500

    Update Regression.py

--- Regression.py
+++ Regression.py
@@ -1,30 +1,12 @@
-import pandas as pd
-import numpy as np
+import pandas
+from pandas.tools import plotting
+from statsmodels.formula.api import ols
 import matplotlib.pyplot as plt
 
-from sklearn import linear_model
-from sklearn import datasets
+data=pandas.read_csv('/Users/John/Desktop/Airline.csv',index_Col=0)
+Y = data['recommended']
+X = data [['rating_valuemoney','rating_inflightEnt','rating_cabinstaff','rating_seatcomfort','rating_foodbeverage',,'rating_overall']]
+model = ols(Y,X).fit()
 
-#Load Train and Test datasets
-#Identify feature and response variable(s) and values must be numeric and numpy arrays
-x_train=input_variables_values_training_datasets
-y_train=target_variables_values_training_datasets
-x_test=input_variables_values_test_datasets
+print(model.summary())
 
-
-# Create linear regression object
-linear = linear_model.LinearRegression()
-
-
-# Train the model using the training sets and check score
-linear.fit(x_train, y_train)
-linear.score(x_train, y_train)
-
-
-#Equation coefficient and Intercept
-print('Coefficient: \n', linear.coef_)
-print('Intercept: \n', linear.intercept_)
-
-
-#Predict Output
-predicted= linear.predict(x_test)
