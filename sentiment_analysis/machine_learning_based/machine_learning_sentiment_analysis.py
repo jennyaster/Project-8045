fa1538d9e0445a59b6a8b178e8520968b6fb8db1
Tung Dao <daothanhtung94@gmail.com>
2017-02-25 01:36:33 -0500

    Update machine_learning_sentiment_analysis.py

--- sentiment_analysis/machine_learning_based/machine_learning_sentiment_analysis.py
+++ sentiment_analysis/machine_learning_based/machine_learning_sentiment_analysis.py
@@ -1,3 +1,5 @@
+#REQUIRED PYTHON 2
+
 import nltk 
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
@@ -5,21 +7,53 @@ import os
 from nltk.corpus.reader import CategorizedPlaintextCorpusReader
 from nltk.util import ngrams
 
+'''Reviews are first classified by rating_overall'''
+import csv
+
+path = 'airline_review_for_ml.csv'
+
+count = 0
+count_pos = 0
+count_neg = 0
+
+with open(path, 'r') as input:
+    review_reader = csv.DictReader(input, delimiter = ",", skipinitialspace = True)
+    for count,row in enumerate(review_reader):
+        
+        filename = "review" + str(count)
+        
+        if row['rating_overall'] in ("6", "7", "8", "9", "10") and count_pos < 15000:
+            output = open('airline_review\\pos\\' + \
+                          filename + '.txt', 'w')
+            output.write(row['reviewcontent'])
+            output.close()
+            count_pos += 1
+            
+        elif row['rating_overall'] in ("1", "2", "3", "4", "5") and count_neg < 15000:
+            output = open('airline_review\\neg\\' + \
+                          filename + '.txt', 'w')
+            output.write(row['reviewcontent'])
+            output.close()
+            count_neg += 1
+        
+
 os.chdir("E:/Documents/GSU/Python Development/Unstructured Data/Team Project/machine_learning_text_analysis")
 reader = CategorizedPlaintextCorpusReader('./airline_review', r'.*\.txt',
                                           cat_pattern = r'(\w+)/*') # file name format
 
-# Positive reviews 
+# Positive reviews file ids
 pos_ids = reader.fileids('pos')
 
-# Negative reviews
+# Negative reviews file ids
 neg_ids = reader.fileids('neg')
 
+'''Generating word feature list'''
 def word_feats(words):
     return dict([(word, True) for word in words])
 
 
-'''Building positive and negative feature lists'''
+'''Building positive and negative feature lists. Each 
+item is the positive/negative word features for a review file'''
 pos_feat = [(word_feats(reader.words(fileids = f)), 'pos')
             for f in pos_ids]
 neg_feat = [(word_feats(reader.words(fileids = f)), 'neg')
@@ -108,6 +142,9 @@ for file in neg_ids[:15000]:
     neg_feat.append(list(review))
 
 # training and testing
+'''Using the first 10000 positive word feature items and the first 10000 negative 
+word feature items. the remaining 5000 positive and 5000 negative feature items are
+used as test dataset'''
 train = pos_feat[:10000] + neg_feat[:10000]
 test = pos_feat[10000:] + neg_feat[10000:]
 
